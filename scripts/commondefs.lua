d27f66081ffc844c3d281f20be1aea5b8075f7d6
Hekateras <39839566+Hekateras@users.noreply.github.com>
2021-01-31 16:42:40 +0100

    Update commondefs.lua

--- scripts/commondefs.lua
+++ scripts/commondefs.lua
@@ -5,7 +5,14 @@ local function onAgentTooltip( tooltip, unit )
 
 end
 
+local function onGuardTooltip( tooltip, unit )
+	if unit:getTraits().mischiefMarked then
+		tooltip:addAbility( STRINGS.TRANSISTOR.AGENTDAEMONS.XU.MARKED_UNIT_NAME, STRINGS.TRANSISTOR.AGENTDAEMONS.XU.MARKED_UNIT_DESC, "gui/icons/item_icons/items_icon_small/icon-item_emp_small.png" )
+	end
+end
+
 return 
 {
 	onAgentTooltip = onAgentTooltip,
+	onGuardTooltip = onGuardTooltip,
 }
